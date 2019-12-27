52910b6145afa770ed5776f933922b8620396d17
iichan2 <hanikanda@yahoo.co.jp>
2019-12-27 22:14:58 +0900

    commit

--- app/javascript/components/Header.vue
+++ app/javascript/components/Header.vue
@@ -2,8 +2,8 @@
 <div>
         <nav class="navbar-light" style="background-color: #e3f2fd;">
             <a class="navbar-brand" href="#">三択クイズ</a>
-            <router-link to="/create" class="btn btn-outline-success" type="button">問題の登録</router-link>
-            <router-link to="/signup" class="btn btn-sm btn-outline-secondary" type="button" v-if="!signedIn">ユーザー登録</router-link>
+            <router-link to="/create" v-if="signedIn" class="btn btn-outline-success" type="button">問題の登録</router-link>
+            <!-- <router-link to="/signup" class="btn btn-sm btn-outline-secondary" type="button" v-if="!signedIn">ユーザー登録</router-link> -->
             <router-link to="/signin" v-if="!signedIn" class="btn btn-sm btn-outline-secondary" type="button">  サインイン</router-link>
             <a href="/" v-if="signedIn" @click="signOut" class="btn btn-sm btn-outline-secondary" type="button">サインアウト</a>
         </nav>

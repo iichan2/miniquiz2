fb3c16a116cd61c32c25718ef7f3d148d2c3e581
iichan2 <hanikanda@yahoo.co.jp>
2019-12-30 15:22:59 +0900

    commit

--- app/javascript/components/Header.vue
+++ app/javascript/components/Header.vue
@@ -3,7 +3,7 @@
         <nav class="navbar-light" style="background-color: #e3f2fd;">
             <a class="navbar-brand" href="#">三択クイズ</a>
             <router-link to="/create" v-if="signedIn" class="btn btn-outline-success" type="button">問題の登録</router-link>
-            <!-- <router-link to="/signup" class="btn btn-sm btn-outline-secondary" type="button" v-if="!signedIn">ユーザー登録</router-link> -->
+            <router-link to="/signup" class="btn btn-sm btn-outline-secondary" type="button" v-if="!signedIn">ユーザー登録</router-link>
             <router-link to="/signin" v-if="!signedIn" class="btn btn-sm btn-outline-secondary" type="button">  サインイン</router-link>
             <a href="/" v-if="signedIn" @click="signOut" class="btn btn-sm btn-outline-secondary" type="button">サインアウト</a>
         </nav>
